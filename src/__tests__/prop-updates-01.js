6cfe85ec1227bdff56a599bf1e319d8a30d9fbe7
TudorTacal <tudortacal@gmail.com>
2020-05-23 21:41:32 +0100

    test element is not rendered in dom (null)

--- src/__tests__/prop-updates-01.js
+++ src/__tests__/prop-updates-01.js
@@ -4,13 +4,12 @@ import {render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
 test('entering an invalid value shows an error message', () => {
-  const {getByLabelText, getByRole, rerender, debug} = render(
+  const {getByLabelText, getByRole, queryByRole, rerender} = render(
     <FavoriteNumber />,
   )
   const input = getByLabelText(/favorite number/i)
   user.type(input, '10')
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
-  debug()
   rerender(<FavoriteNumber max={10} />)
-  debug()
+  expect(queryByRole('alert')).toBeNull()
 })
